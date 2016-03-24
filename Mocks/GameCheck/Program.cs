795a20f04155f5f2960bda1817d99cfc0213c49a
sslebedev <ssergejslebedev@gmail.com>
2016-03-24 15:07:16 +0300

    GameCheck basic mock

--- Mocks/GameCheck/Program.cs
+++ Mocks/GameCheck/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace GameCheck
 {
     class Program
     {
+        struct KeyValues
+        {
+            public string grp;
+            public string gsp;
+            public string rf;
+
+            public bool Fullfilled
+            {
+                get 
+                {
+                    return !string.IsNullOrEmpty(grp) &&
+                        !string.IsNullOrEmpty(gsp) &&
+                        !string.IsNullOrEmpty(rf); 
+                }
+            }
+        }
+
+        private class UnknownKeyException : Exception
+        {
+            public UnknownKeyException(string message)
+                :base(message)
+            {}
+        }
+
+
+        // Valid parameter set:
+        // "-grp \"{0}\" -gsp \"{1}\" -rf \"{2}\""
+        // Where :
+        // {0} -- pathProjects = Path.Combine(pathRelease, "projects");
+        // {1} -- srcInfo = new DirectoryInfo(pathProjects); gsp = srcInfo.GetDirectories().FirstOrDefault(d => d.Name.StartsWith("Game")).FullName;
+        // {2} -- checkResult = Application.LocalUserAppDataPath + "\\gameCheckResult.xml";
         static void Main(string[] args)
         {
+            var values = new KeyValues();
+            try {
+                for (var i = 0; i < 6; ++i) {
+                    var key = ((string)args.GetValue(i)).Trim();
+                    switch (key) {
+                        case "-grp":
+                            values.grp = args.GetValue(++i) as string;
+                            break;
+                        case "-gsp":
+                            values.gsp = args.GetValue(++i) as string;
+                            break;
+                        case "-rf":
+                            values.rf = args.GetValue(++i) as string;
+                            break;
+                        default:
+                            throw new UnknownKeyException("Unknown key '" + key + "'");
+                    }
+                }
+            } catch (IndexOutOfRangeException e) {
+                Console.WriteLine("Error: " + e.Message);
+                return;
+            } catch (UnknownKeyException e) {
+                Console.WriteLine("Error: " + e.Message);
+                return;
+            }
+
+            if (!values.Fullfilled) {
+                Console.WriteLine("Error: " + "Not enough keys");
+                return;
+            }
+
+            // In this version gsp and grp considered always correct
+            Debug.Assert(values.rf != null, "values.rf != null");
+            File.WriteAllText(values.rf, Properties.Resources.gameCheckResultValid);
         }
     }
 }
